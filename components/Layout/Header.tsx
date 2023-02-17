010e669c1258553ea05f10bd213999792eee223c
Tahlil <bsse0803@iit.du.ac.bd>
2023-02-17 16:37:10 +0600

    Fix user profile bug

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,36 +1,52 @@
-import Link from 'next/link';
-import { Box, Flex, Button, HStack, Container, useBoolean, Image } from '@chakra-ui/react';
-import { useState, useEffect } from 'react';
+import Link from "next/link";
+import {
+  Box,
+  Flex,
+  Button,
+  HStack,
+  Container,
+  useBoolean,
+  Image,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
-import { IoMenu } from 'react-icons/io5';
-import { useAuthStore } from 'store/AuthStore';
-import MobileDrawer from './MobileDrawer';
-import Logo from '../Logo/Logo';
-import { MenuLinks } from './MenuLinks';
-import ProfileMenu from './ProfileMenu';
+import { IoMenu } from "react-icons/io5";
+import { useAuthStore } from "store/AuthStore";
+import MobileDrawer from "./MobileDrawer";
+import Logo from "../Logo/Logo";
+import { MenuLinks } from "./MenuLinks";
+import ProfileMenu from "./ProfileMenu";
 
 const Header = () => {
   const [drawerOpen, setDrawerOpen] = useBoolean(false);
   const { token, logout } = useAuthStore();
-  const [background, setBackground] = useState(false)
+  const [background, setBackground] = useState(false);
 
   const changeBackground = () => {
-    console.log(window.scrollY)
+    console.log(window.scrollY);
     if (window.scrollY >= 66) {
-      setBackground(true)
+      setBackground(true);
     } else {
-      setBackground(false)
+      setBackground(false);
     }
-  }
+  };
 
   useEffect(() => {
-    changeBackground()
+    changeBackground();
     // adding the event when scroll change background
-    window.addEventListener("scroll", changeBackground)
-  })
-  // 
+    window.addEventListener("scroll", changeBackground);
+  });
+  //
   return (
-    <Box position="fixed" top="0" left="0" right="0" zIndex="999" background={background? '#975A16' : 'none' } transition="all 1s ease">
+    <Box
+      position="fixed"
+      top="0"
+      left="0"
+      right="0"
+      zIndex="999"
+      background={background ? "#975A16" : "none"}
+      transition="all 1s ease"
+    >
       <Container maxW="container.xl" px="0" position="relative">
         <Image
           src="images/shapes/rectangle.png"
@@ -45,22 +61,32 @@ const Header = () => {
               <Logo />
             </Link>
 
-            <HStack gap={7} display={{ base: 'none', md: 'flex'}}>
-              <MenuLinks />
-              <ProfileMenu />
-            </HStack>
-
             {token ? (
-              <Button display={{ base: 'none', md: 'flex' }} onClick={logout}>
+              <>
+                <HStack gap={7} display={{ base: "none", md: "flex" }}>
+                  <MenuLinks />
+                  <ProfileMenu />
+                </HStack>
+              <Button display={{ base: "none", md: "flex" }} onClick={logout}>
                 Logout
               </Button>
+              </>
             ) : (
-              <Link href="/login">
-                <Button display={{ base: 'none', md: 'flex' }}>Login</Button>
-              </Link>
+              <>
+                <HStack gap={7} display={{ base: "none", md: "flex" }}>
+                  <MenuLinks />
+                </HStack>
+                <Link href="/login">
+                  <Button display={{ base: "none", md: "flex" }}>Login</Button>
+                </Link>
+              </>
             )}
 
-            <Button variant="link" onClick={setDrawerOpen.on} display={{ md: 'none' }}>
+            <Button
+              variant="link"
+              onClick={setDrawerOpen.on}
+              display={{ md: "none" }}
+            >
               <IoMenu size="30" />
             </Button>
           </Flex>
